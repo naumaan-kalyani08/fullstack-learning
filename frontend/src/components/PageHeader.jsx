ac4a11cb5a7c78cefe5e8cd6d4db72a3b2250b93
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2025-09-02 22:33:23 +0530

    handled routes in the header

--- frontend/src/components/PageHeader.jsx
+++ frontend/src/components/PageHeader.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Layout } from "antd";
+import { Link } from "react-router";
 
 const { Header } = Layout;
 const PageHeader = () => {
@@ -8,9 +9,18 @@ const PageHeader = () => {
       <div className="text-xl font-semibold text-gray-800">My App</div>
       {/* You can add navigation or actions here */}
       <div className="space-x-4">
-        <button className="text-gray-700 hover:text-blue-500">Home</button>
-        <button className="text-gray-700 hover:text-blue-500">About</button>
-        <button className="text-gray-700 hover:text-blue-500">Contact</button>
+        <Link to="/" className="text-gray-700 hover:text-blue-500">
+          Home
+        </Link>
+        <Link to="/about" className="text-gray-700 hover:text-blue-500">
+          About
+        </Link>
+        <Link to="/contact-us" className="text-gray-700 hover:text-blue-500">
+          Contact
+        </Link>
+        <Link to="/login" className="text-gray-700 hover:text-blue-500">
+          Logout
+        </Link>
       </div>
     </Header>
   );
