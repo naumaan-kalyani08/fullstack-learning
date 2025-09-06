4ad5b404fbad14977307479172f55d0b6a393817
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2025-09-06 15:55:58 +0530

    daily commits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
-import WithHeaderFooter from "./CommonLayouts/withHeaderFooter";
+import MainRouter from "./PageRouter/Routes";
 import "./App.css";
 const App = () => {
   return (
     <div>
-      <WithHeaderFooter>
-        <h1 className="bg-red-100">Welcome to the App</h1>
-      </WithHeaderFooter>
+      <MainRouter />
     </div>
   );
 };
