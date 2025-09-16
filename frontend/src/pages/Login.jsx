42f0b466e4fd0b4bc4bf4551bf6a4047fdedd978
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2025-09-16 20:48:22 +0530

    daily commit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,12 @@
+import { Card } from "antd";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login-card-container bg-red-200 ">
+      <Card> hello</Card>
+      <div className="login-card-main bg-white">Login</div>
+    </div>
+  );
 };
 
 export default Login;
