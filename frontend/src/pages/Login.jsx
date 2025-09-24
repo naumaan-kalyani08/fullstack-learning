92920f4f42f6ccbd4ea12a58a88821623a94026a
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2025-09-24 21:51:21 +0530

    started working on login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,15 @@
-import { Card } from "antd";
+import { Button, Card, Input } from "antd";
 
 const Login = () => {
   return (
     <div className="login-card-container bg-red-200 ">
-      <Card> hello</Card>
+      <Card title="Login" className="login-card-main bg-white">
+        <Input placeholder="email" />
+        <Input placeholder="Password" />
+        <div className="">
+          <Button type="primary">Login</Button>
+        </div>
+      </Card>
       <div className="login-card-main bg-white">Login</div>
     </div>
   );
