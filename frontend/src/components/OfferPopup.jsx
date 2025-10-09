a7ea7527c2e7d3eec39b518b1f219acfb3d10479
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2025-10-09 20:50:54 +0530

    created a offer popup

---
+++ frontend/src/components/OfferPopup.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Modal, Typography, Button, Space } from "antd";
+import { GiftOutlined } from "@ant-design/icons";
+
+const { Title, Paragraph } = Typography;
+
+const OfferPopup = ({ open, onClose }) => {
+  return (
+    <Modal
+      open={open}
+      onCancel={onClose}
+      footer={null}
+      centered
+      width={400}
+    >
+      <div className="text-center py-6">
+        <Space direction="vertical" size="large" className="w-full">
+          <GiftOutlined style={{ fontSize: "48px", color: "#1890ff" }} />
+          <Title level={3}>Special Welcome Offer!</Title>
+          <Paragraph>
+            Get 20% off on your first purchase by using code{" "}
+            <strong>WELCOME20</strong>
+          </Paragraph>
+          <Button type="primary" size="large" onClick={onClose}>
+            Shop Now
+          </Button>
+        </Space>
+      </div>
+    </Modal>
+  );
+};
+
+export default OfferPopup;
